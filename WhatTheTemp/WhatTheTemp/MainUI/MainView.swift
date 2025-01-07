aeb9153cbb704db6d6289d1e76cb761741854f31
zzangmmz <leemj036@gmail.com>
2025-01-08 00:48:10 +0900

    [Feat] 날씨 요소들 세로 스택뷰 만드는 makeVerticalStackView() 메서드 생성

--- WhatTheTemp/WhatTheTemp/MainUI/MainView.swift
+++ WhatTheTemp/WhatTheTemp/MainUI/MainView.swift
@@ -94,6 +94,15 @@ final class MainView: UIView {
         return imageView
     }
     
+    private func makeVerticalStackView(with views: [UIView]) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: views)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 5
+        return stackView
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
