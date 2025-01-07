f389d889b9ec66643d2e62e3f1d07067c2428174
sonny <gyeomsony@gmail.com>
2025-01-07 21:11:35 +0900

    [Chore] 기본세팅

---
+++ WhatTheTemp/WhatTheTemp/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  WhatTheTemp
+//
+//  Created by 손겸 on 1/7/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
+
---
+++ WhatTheTemp/WhatTheTemp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WhatTheTemp
+//
+//  Created by 손겸 on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
