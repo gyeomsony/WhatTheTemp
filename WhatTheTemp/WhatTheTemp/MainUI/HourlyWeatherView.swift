a3a5ad25016371d2f6e926c2a0b819d5a9849bb6
zzangmmz <leemj036@gmail.com>
2025-01-08 04:19:02 +0900

    [Feat] 버튼 타이을 목록 순서대로 버튼 만드는 setupButtons() 메서드 구현

--- WhatTheTemp/WhatTheTemp/MainUI/HourlyWeatherView.swift
+++ WhatTheTemp/WhatTheTemp/MainUI/HourlyWeatherView.swift
@@ -12,9 +12,22 @@ final class HourlyWeatherView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButtons() {
+        let buttonTitles = ["Today", "Tomorrow", "Next 3 Days"]
+        for (index, title) in buttonTitles.enumerated() {
+            let button = UIButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(.gray, for: .normal)
+            button.setTitleColor(.white, for: .selected)
+            button.tag = index
+            buttons.append(button)
+        }
     }
 }
