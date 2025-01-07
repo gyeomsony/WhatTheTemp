91388d4371af56c03e0cdd68ffc88d153aa91ad1
zzangmmz <leemj036@gmail.com>
2025-01-08 01:40:54 +0900

    [Refactor] 커스텀 이미지뷰 만드는 메서드 windIconImageView() 삭제 후 커스텀 이미지뷰 클래스 IconImageView 생성

---
+++ WhatTheTemp/WhatTheTemp/MainUI/IconImageView.swift
@@ -0,0 +1,20 @@
+//
+//  IconImageView.swift
+//  WhatTheTemp
+//
+//  Created by 이명지 on 1/8/25.
+//
+
+import UIKit
+
+final class IconImageView: UIImageView {
+    init(image: String) {
+        super.init(frame: .zero)
+        self.image = UIImage(named: "Wind")
+        self.contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
