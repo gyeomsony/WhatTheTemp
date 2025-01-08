720f70268e14f29fd4f6c4e0742198c84c18eb26
zzangmmz <leemj036@gmail.com>
2025-01-08 18:29:58 +0900

    [Feat] 컬렉션뷰로 페이지네이션 구현하기 위해 WeatherViewController 생성, 컬렉션뷰 pageCollectionView 생성

---
+++ WhatTheTemp/WhatTheTemp/MainUI/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherViewController.swift
+//  WhatTheTemp
+//
+//  Created by 이명지 on 1/8/25.
+//
+
+import UIKit
+
+final class WeatherViewController: UIViewController {
+    private var pageCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+}
