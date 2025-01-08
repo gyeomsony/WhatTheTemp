9dc7b4d703d5c57ed224eb84a10f391f699d3da4
zzangmmz <leemj036@gmail.com>
2025-01-08 13:03:05 +0900

    [Feat] HourlyCollectionViewCell 파일 생성 후 hourLabel 정의

---
+++ WhatTheTemp/WhatTheTemp/MainUI/CustomView/HourlyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HourlyCollectionViewCell.swift
+//  WhatTheTemp
+//
+//  Created by 이명지 on 1/8/25.
+//
+
+import UIKit
+import SnapKit
+
+final class HourlyCollectionViewCell: UICollectionViewCell {
+    private let hourLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+}
