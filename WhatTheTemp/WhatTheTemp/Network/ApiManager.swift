483aaa82ac28d028954108dd7ed4e79681e77300
sonny <gyeomsony@gmail.com>
2025-01-09 00:48:27 +0900

    [Feat] ApiManager 구현

---
+++ WhatTheTemp/WhatTheTemp/Network/ApiManager.swift
@@ -0,0 +1,51 @@
+//
+//  ApiManager.swift
+//  WhatTheTemp
+//
+//  Created by 손겸 on 1/8/25.
+//
+
+import Foundation
+import Moya
+
+enum WeatherAPI {
+    case oneCall(lat: Double,      // 위도
+                 lon: Double,      // 경도
+                 exclude: String,  // 제외할 항목
+                 units: String)    // 단위 (metric = 온도 섭씨 (°C))
+}
+
+extension WeatherAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://api.openweathermap.org/data/3.0")!
+    }
+    
+    var path: String {
+        switch self {
+        case .oneCall:
+            return "/onecall"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case let .oneCall(lat, lon, exclude, units):
+            let parameters: [String: Any] = [
+                "lat": lat,
+                "lon": lon,
+                "exclude": exclude,
+                "units": units,
+                "appid": "8ef68fb1d27fb92f55802750b90d54f7"
+            ]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        ["Content-Type": "application/json"]
+    }
+}
