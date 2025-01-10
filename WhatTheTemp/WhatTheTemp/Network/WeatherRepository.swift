43581bc85420aff2d92324b5e122ecdc45111f13
emilyj4482 <emilyj4482@gmail.com>
2025-01-10 19:17:22 +0900

    [Refactor] fetchWeather 함수가 Single을 반환하도록 수정

--- WhatTheTemp/WhatTheTemp/Network/WeatherRepository.swift
+++ WhatTheTemp/WhatTheTemp/Network/WeatherRepository.swift
@@ -8,19 +8,27 @@
 import Foundation
 import RxMoya
 import Moya
+import RxSwift
 
 final class WeatherRepository {
     private let provider = MoyaProvider<WeatherAPI>()
     
-    func fetchWeather() {
-        provider.rx.request(.oneCall(lat: 37.5665851, lon: 126.9782038))
-            .subscribe { event in
-                switch event {
-                case .success(let response):
-                    print(response)
-                case .failure(let error):
-                    print(error)
+    func fetchWeather(lat: Double, lon: Double) -> Single<WeatherResponse> {
+        return Single.create { [weak self] observer in
+            let disposable = self?.provider.rx.request(.oneCall(lat: lat, lon: lon))
+                .map(WeatherResponse.self)
+                .subscribe { event in
+                    switch event {
+                    case .success(let response):
+                        return observer(.success(response))
+                    case .failure(let error):
+                        return observer(.failure(error))
+                    }
                 }
+            guard let disposable = disposable else {
+                return Disposables.create()
             }
+            return disposable
+        }
     }
 }
