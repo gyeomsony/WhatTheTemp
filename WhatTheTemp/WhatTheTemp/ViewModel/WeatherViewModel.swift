bb5531557ac5bfd2c0c4cfdf72bb248ca7c7b559
emilyj4482 <emilyj4482@gmail.com>
2025-01-13 19:35:28 +0900

    [Feat] Today hourly weather 방출 구현

--- WhatTheTemp/WhatTheTemp/ViewModel/WeatherViewModel.swift
+++ WhatTheTemp/WhatTheTemp/ViewModel/WeatherViewModel.swift
@@ -14,6 +14,7 @@ class WeatherViewModel {
     private let disposeBag = DisposeBag()
     
     let currentWeather = PublishRelay<Current>()
+    let hourlyWeather = PublishRelay<[WeatherSummary]>()
     
     init(repository: WeatherRepositoryProtocol) {
         self.repository = repository
@@ -21,7 +22,7 @@ class WeatherViewModel {
     
     func fetchWeatherResponse(lat: Double, lon: Double) {
         repository.fetchWeather(lat: lat, lon: lon)
-            .map { response -> Current in
+            .map { response -> (Current, [WeatherSummary] ) in
                 let current = Current(weatherDescription: response.currentWeather.weather[0].description,
                                       currentTemperature: response.currentWeather.temperature,
                                       weatherCode: response.currentWeather.weather[0].code,
@@ -31,10 +32,20 @@ class WeatherViewModel {
                                       windSpeed: response.currentWeather.windSpeed,
                                       humidity: response.currentWeather.humidity,
                                       rainProbability: response.hourlyWeather[0].rain)
-                return current
+                
+                // 현재 "시"
+                let hour = Date.now.hour
+                
+                /// 현재 시 ~ 오늘 23시까지 hourly weather
+                let weatherSummaries = response.hourlyWeather[0...23-hour].map { hourlyWeather in
+                    WeatherSummary(time: hourlyWeather.dateTime.hour, statusCode: hourlyWeather.weather[0].code, temperature: hourlyWeather.temperature)
+                }
+                
+                return (current, weatherSummaries)
             }
-            .subscribe(onSuccess: { [weak self] current in
+            .subscribe(onSuccess: { [weak self] (current, weatherSummaries) in
                 self?.currentWeather.accept(current)
+                self?.hourlyWeather.accept(weatherSummaries)
             }, onFailure: { error in
                 print("에러 발생: \(error)")
             })
