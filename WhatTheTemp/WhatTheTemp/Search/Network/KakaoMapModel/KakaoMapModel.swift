d93165450507cf83bbdc50802dc931673ec62077
sy0201 <noey0201@gmail.com>
2025-01-14 13:10:26 +0900

    [Fix] cityName 저장 안되던 문제 수정

--- WhatTheTemp/WhatTheTemp/Search/Network/KakaoMapModel/KakaoMapModel.swift
+++ WhatTheTemp/WhatTheTemp/Search/Network/KakaoMapModel/KakaoMapModel.swift
@@ -11,10 +11,9 @@ import Foundation
 struct KakaoMapModel: Decodable {
     let documents: [Document]
     let meta: Meta
-
+    
     struct Document: Decodable {
         let addressName: String
-        let cityName: String? // 행정구역"시"까지 데이터
         let lon: String
         let lat: String
         
@@ -25,15 +24,31 @@ struct KakaoMapModel: Decodable {
         var latAsDouble: Double? {
             return Double(lat)
         }
-
+        
+        // address 객체를 추가하여 region_2depth_name을 포함
+        struct Address: Decodable {
+            let region2DepthName: String?
+            
+            enum CodingKeys: String, CodingKey {
+                case region2DepthName = "region_2depth_name"
+            }
+        }
+        
+        let address: Address
+        
+        // cityName은 address.region2DepthName을 통해 가져옴
+        var cityName: String? {
+            return address.region2DepthName
+        }
+        
         enum CodingKeys: String, CodingKey {
             case addressName = "address_name"
-            case cityName = "region_2depth_name"
             case lon = "x"
             case lat = "y"
+            case address // address 객체를 매핑
         }
     }
-
+    
     struct Meta: Decodable {
         let pageableCount: Int
         let totalCount: Int
